3b6a87ae088645436b155ffc81e8f271a79df15a
Sergey Karayev <sergeykarayev@gmail.com>
2013-06-13 01:54:36 -0700

    master

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+CUR_BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ $CUR_BRANCH='source' ]; then
+    git push origin source
+
+    jekyll build
+    git checkout master
+    git rm -qr .
+    cp -r _site/. .
+    rm -r _site
+    git add -A
+    git commit -am `git rev-parse --abbrev-ref HEAD`
+    git push origin master
+fi
